8bd4c87d9119a0c2f573ebd3da0ead99ea2d4963
lamvwl <lamvwl@users.noreply.github.com>
2020-07-12 19:26:06 -0400

    Filling out routes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var mongodb = require('mongodb');
 var app = express();
 
 var MONGODB_URI = 'mongodb://127.0.0.1:27017'
-var app, db, coll; 
+var app, db, coll;
+var resultsLimit = 5 
   
 mongodb.MongoClient.connect(MONGODB_URI, {
     useNewUrlParser: true,
@@ -19,7 +20,6 @@ mongodb.MongoClient.connect(MONGODB_URI, {
     });
 });  
 
-
 app.get('/findone', function(req, res) { 
     coll.findOne({}, function(err, doc) {
       console.log(doc);
@@ -27,4 +27,25 @@ app.get('/findone', function(req, res) {
     });
 });
 
+app.get('/findall', function(req, res) { 
+    coll.find({}).toArray(function(err, docs) {
+      console.log(docs);
+      res.send(docs);
+    });
+});
 
+app.get('/findcity*', function(req, res) { 
+    const query = req.query.query;
+    const limit = Number(req.query.limit) || resultsLimit;
+    coll.find({city: {$regex: query, $options:'i'}}).limit(limit).toArray(function(err, docs) {
+      res.send(docs);
+    });
+});
+
+app.get('/findstate*', function(req, res) { 
+    const query = req.query.query;
+    const limit = Number(req.query.limit) || resultsLimit;
+    coll.find({state: {$regex: query, $options:'i'}}).limit(limit).toArray(function(err, docs) {
+      res.send(docs);
+    });
+});
