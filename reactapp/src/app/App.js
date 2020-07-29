3bd000ad2cf7e8c3dca8c8b23bccc17e341f1527
lamvwl <lamvwl@users.noreply.github.com>
2020-07-29 14:58:46 -0400

    Added dropdown; Toggles; Animations; Alignment

--- reactapp/src/app/App.js
+++ reactapp/src/app/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { MemoryRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
 import { FindOne, FindAll, FindCity, FindState } from '../pages'
+
+import Toggle from '../Toggle.js'
 
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import { LinkContainer } from 'react-router-bootstrap';
 
-import { Nav, Navbar, NavDropdown, Form, FormControl, Modal, Row, Col } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown, Form, FormControl, Modal, Row, Col, Dropdown, DropdownButton } from 'react-bootstrap'
 import './App.css';
 
 const Home = () => <span></span>;
 
-const Findone = () => <span>FindOne</span>;
-
-const Findall = () => <span>FindAll</span>;
-const FindByCity = () => <span>FindByCity</span>;
-const FindByState = () => <span>FindByState</span>;
-
-
-
+const Findone = () => <span>Find One Zipcode</span>;
+const Findall = () => <span>Find All</span>;
+const FindByCity = () => <span>Find By City</span>;
+const FindByState = () => <span>Find By State</span>;
 
 function MyVerticallyCenteredModal(props) {
   return (
@@ -60,38 +57,52 @@ function MyVerticallyCenteredModal(props) {
                   </Switch>
             </h2>
       <Modal.Body>
-        <p>
-            <Container className="outerContainer">
-              <Jumbotron>
-                  <Switch>
-                    <Route path="/findall" exact component={FindAll} />
-                    <Route path="/findone" exact component={FindOne} />
-                    <Route path="/findcity" exact component={FindCity} />
-                    <Route path="/findstate" exact component={FindState} />
-                  </Switch>
-                <h4>
-                 {' '}
-                  <ButtonToolbar className="custom-btn-toolbar" style={{display: 'flex', justifyContent: 'center'}}>
-                    <LinkContainer to="/home">
-                      <Button>Home</Button>
+      <p>
+        <Container className="outerContainer">
+          <Jumbotron id="jumbotron">
+            <Container fluid>
+              <Row>
+                <Col id="col-dropdown-access-button">
+                  <DropdownButton  title="Access Zip Codes" id="btn-dropdown-toggle-access">
+                    <LinkContainer to="#">
+                      <Dropdown.Item to="#" onClick={props.onHide}>Home</Dropdown.Item>
                     </LinkContainer>
                     <LinkContainer to="/findone">
-                      <Button>Sample One Zip Code</Button>
+                      <Dropdown.Item to="/findone">Sample One Zip Code</Dropdown.Item>
                     </LinkContainer>
                     <LinkContainer to="/findall">
-                      <Button>All Zip Codes</Button>
+                      <Dropdown.Item href="/findall">All Zip Codes</Dropdown.Item>
                     </LinkContainer>
                     <LinkContainer to="/findcity">
-                      <Button>Find Zip Codes by City</Button>
+                      <Dropdown.Item href="/findCity">Find Zip Codes by City</Dropdown.Item>
                     </LinkContainer>
                     <LinkContainer to="/findstate">
-                      <Button>Find Zip Codes by State</Button>
+                      <Dropdown.Item href="/findSTate">Find Zip Codes by State</Dropdown.Item>
                     </LinkContainer>
-                  </ButtonToolbar>
-                </h4>
-              </Jumbotron>
+                  </DropdownButton>
+                </Col>
+                <Col xs={8} id="col-results">
+                  <label htmlFor="material-switch">
+                    <span>Query </span>
+                    <Toggle />
+                    <span>Limit </span>
+                    <Toggle />
+                  </label>
+                <Switch>
+                  <Route path="/findall" exact component={FindAll} />
+                  <Route path="/findone" exact component={FindOne} />
+                  <Route path="/findcity" exact component={FindCity} />
+                  <Route path="/findstate" exact component={FindState} />
+                </Switch>
+                </Col>
+              </Row>
             </Container>
-        </p>
+          <h4>
+          {' '}
+          </h4>
+          </Jumbotron>
+        </Container>
+      </p>
       </Modal.Body>
           </MemoryRouter>
       <Modal.Footer>
@@ -107,8 +118,6 @@ function App() {
    const [modalShow, setModalShow] = React.useState(false);
   return (
     <div>
-
-
       <Navbar bg="light" expand="lg" id="navbar">
         <Navbar.Brand href="https://react-bootstrap.github.io">Powered by React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -116,12 +125,8 @@ function App() {
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/">Abracadabra</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown title="Databases" id="basic-nav-dropdown">
+              <NavDropdown.Item href="">Zipcodes</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
